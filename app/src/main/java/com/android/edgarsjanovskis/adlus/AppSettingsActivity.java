7f0dbffa682c77a273ddabafce51b8cf857ee2e5
Edgars Janovskis <edgars@enclatvia.lv>
2017-03-02 17:49:07 +0200

    Added TimePicker and implemented in AppSettingsActivity

--- app/src/main/java/com/android/edgarsjanovskis/adlus/AppSettingsActivity.java
+++ app/src/main/java/com/android/edgarsjanovskis/adlus/AppSettingsActivity.java
@@ -11,24 +11,32 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-import java.util.Calendar;
-
+//import java.util.Calendar;
 
 public class AppSettingsActivity extends AppCompatActivity {
 
 
     public final String USER_EMAIL = "User_email";
     public final String SERVER_URL = "Server_URL";
-    public final String APP_START_TIME = "App_start_time";
-    public final String APP_STOP_TIME = "App_stop_time";
-    private Calendar calendar;
+    public final String APP_START_TIME_HOUR = "startHour";
+    public final String APP_START_TIME_MINUTE= "startMinute";
+    public final String APP_STOP_TIME_HOUR = "stopHour";
+    public final String APP_STOP_TIME_MINUTE= "stopMinute";
+    public final String TIME_SELECTED = "timeSelected";
+    //private Calendar calendar;
+    private TextView tv1;
+    private TextView tv2;
     private TimePicker tp1;
     private TimePicker tp2;
-    private String startTime;
-    private String stopTime;
     private CheckBox cb;
+    int hoursStart = 0;
+    int minutesStart = 0;
+    int hoursStop =0;
+    int minutesStop = 0;
 
 
     @Override
@@ -40,75 +48,115 @@ public class AppSettingsActivity extends AppCompatActivity {
         //setupUI(findViewById(R.id.parent));
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
         String email = prefs.getString("User_email", " ");
-        String server = prefs.getString("Server_URL", " ");
-        String startTime = prefs.getString("App_start_time", " ");
-        String stopTime = prefs.getString("App_stop_time", " ");
+        final String server = prefs.getString("Server_URL", " ");
         EditText et1 = (EditText) findViewById(R.id.etEmail);
         et1.setText(email);
         EditText et2 = (EditText) findViewById(R.id.etServer);
         et2.setText(server);
-
+        cb = (CheckBox)findViewById(R.id.cbTime);
+        cb.setChecked(prefs.getBoolean("timeSelected", false));
         tp1 = (TimePicker) findViewById(R.id.tpStart);
+        tv1 = (TextView)findViewById(R.id.tv1);
         tp2 = (TimePicker) findViewById(R.id.tpStop);
-        calendar = Calendar.getInstance();
+        tv2 = (TextView)findViewById(R.id.tv2);
+        tp1.is24HourView();
+        tp2.is24HourView();
+        if(cb.isChecked()){
+            tp1.setVisibility(View.VISIBLE);
+            tv1.setVisibility(View.VISIBLE);
+            tp2.setVisibility(View.VISIBLE);
+            tv2.setVisibility(View.VISIBLE);
+        }else
+        {
+            tp1.setVisibility(View.GONE);
+            tv1.setVisibility(View.GONE);
+            tp2.setVisibility(View.GONE);
+            tv2.setVisibility(View.GONE);}
 
-        int hoursStart = calendar.get(Calendar.HOUR_OF_DAY);
-        int minutesStart = calendar.get(Calendar.MINUTE);
-        showStartTime(hoursStart, minutesStart);
-
-        int hoursStop = calendar.get(Calendar.HOUR_OF_DAY);
-        int minutesStop = calendar.get(Calendar.MINUTE);
-        showStartTime(hoursStop, minutesStop);
-    }
-
-    public void setStartTime(View view){
         if(Build.VERSION.SDK_INT < 23){
-            int hoursStart = tp1.getCurrentHour();
-            int minutesStart = tp1.getCurrentMinute();
-            showStartTime(hoursStart, minutesStart);
-
-        } else{
-            int hoursStart = tp1.getHour();
-            int minutesStart = tp1.getMinute();
-            showStartTime(hoursStart, minutesStart);
+            tp1.setCurrentHour(prefs.getInt("startHour", 0));
+            tp1.setCurrentMinute(prefs.getInt("startMinute", 00));
+            tp2.setCurrentHour(prefs.getInt("stopHour", 0));
+            tp2.setCurrentMinute(prefs.getInt("stopMinute", 00));
+        }else {
+            tp1.setHour(prefs.getInt("startHour", 0));
+            tp1.setMinute(prefs.getInt("startMinute", 00));
+            tp2.setHour(prefs.getInt("stopHour", 0));
+            tp2.setMinute(prefs.getInt("stopMinute", 00));
         }
+        //calendar = Calendar.getInstance();
 
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(cb.isChecked()){
+                    tv1.setVisibility(View.VISIBLE);
+                    tp1.setVisibility(View.VISIBLE);
+                    tv2.setVisibility(View.VISIBLE);
+                    tp2.setVisibility(View.VISIBLE);
+                    setStartTime(tp1);
+                    setStopTime(tp2);
+                    SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
+                    prefs.edit().putBoolean(TIME_SELECTED, true).apply();
+                }else{
+                    tv1.setVisibility(View.GONE);
+                    tv2.setVisibility(View.GONE);
+                    tp1.setVisibility(View.GONE);
+                    tp2.setVisibility(View.GONE);
+                    setStartTime(tp1);
+                    setStopTime(tp2);
+                    SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
+                    prefs.edit().putBoolean(TIME_SELECTED, false).apply();
+                }
+            }
+        });
     }
-    public void setStopTime(View view){
-        if(Build.VERSION.SDK_INT < 23){
-            int hoursStop = tp2.getCurrentHour();
-            int minutesStop = tp2.getCurrentMinute();
-            showStopTime(hoursStop, minutesStop);
-        } else{
-            int hoursStop = tp2.getHour();
-            int minutesStop= tp2.getMinute();
-            showStopTime(hoursStop, minutesStop);
-        }
 
-    }
-
-        public void showStartTime(int hoursStart, int minutesStart){
+    public void setStartTime(View view) {
+        if (cb.isChecked()) {
+            if (Build.VERSION.SDK_INT < 23) {
+                hoursStart = tp1.getCurrentHour();
+                minutesStart = tp1.getCurrentMinute();
+            } else {
+                hoursStart = tp1.getHour();
+                minutesStart = tp1.getMinute();
+            }
+        }else {hoursStart = 0; minutesStart = 0;}
             StringBuilder sb1 = new StringBuilder();
-            sb1.append(hoursStart).append(" : ").append(minutesStart);
-            startTime = sb1.toString();
-        }
-        public void showStopTime(int hoursStop, int minutesStop){
-            StringBuilder sb2 = new StringBuilder();
-            sb2.append(hoursStop).append(" : ").append(minutesStop);
-            stopTime = sb2.toString();
+            sb1.append(hoursStart).append(":").append(minutesStart);
+            String startTime = sb1.toString();
+            Toast.makeText(this, "Sākums: " + startTime, Toast.LENGTH_LONG).show();
+            SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
+            prefs.edit().putInt(APP_START_TIME_HOUR, hoursStart).apply();
+            prefs.edit().putInt(APP_START_TIME_MINUTE, minutesStart).apply();
+
         }
 
+    public void setStopTime(View view){
+        if (cb.isChecked()) {
+            if(Build.VERSION.SDK_INT < 23){
+                hoursStop = tp2.getCurrentHour();
+                minutesStop = tp2.getCurrentMinute();
+            } else{
+                hoursStop = tp2.getHour();
+                minutesStop= tp2.getMinute();
+            }
+        }else{hoursStop = 0; minutesStop = 0;}
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(hoursStop).append(":").append(minutesStop);
+        String stopTime = sb2.toString();
+        Toast.makeText(this, "Beigas: " + stopTime, Toast.LENGTH_LONG).show();
+        SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
+        prefs.edit().putInt(APP_STOP_TIME_HOUR, hoursStop).apply();
+        prefs.edit().putInt(APP_STOP_TIME_MINUTE, minutesStop).apply();
+    }
 
     public void buttonSave_onClick (View view){
         String userEmail;
         String serverUrl;
-
-        //if (cb.isChecked()) {
-            SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
-            prefs.edit().putString(APP_START_TIME, startTime).apply();
-            SharedPreferences prefs1 = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
-            prefs1.edit().putString(APP_STOP_TIME, stopTime).apply();
-        //} else {return;}
+        setStartTime(this.tp1);
+        setStopTime(this.tp2);
 
         EditText et1 = (EditText)findViewById(R.id.etEmail);
         et1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -165,19 +213,21 @@ public class AppSettingsActivity extends AppCompatActivity {
         if(serverUrl.equals(" ")) {
             AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
             builder1.setMessage("Lūdzu ievadiet servera adresi un portu");
-            builder1.setCancelable(true);
+            builder1.setCancelable(false);
 
             builder1.setPositiveButton(
                     "LABI",
                     new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {dialog.cancel();
+                        public void onClick(DialogInterface dialog, int id) {
+                            return;
                         }
                     });
 
             builder1.setNegativeButton(
                     "ATLIKT",
                     new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {dialog.cancel();
+                        public void onClick(DialogInterface dialog, int id) {
+                            return;
                         }
                     });
 
@@ -188,11 +238,14 @@ public class AppSettingsActivity extends AppCompatActivity {
             SharedPreferences prefs4 = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
             prefs4.edit().putString(SERVER_URL, serverUrl).apply();
         }
+
+        finish();
     }
 
-        public void hideKeyboard(View view) {
+    public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+
 
 }
--- app/src/main/java/com/android/edgarsjanovskis/adlus/PhoneInfoActivity.java
+++ app/src/main/java/com/android/edgarsjanovskis/adlus/PhoneInfoActivity.java
@@ -7,10 +7,7 @@ import android.widget.TextView;
 
 public class PhoneInfoActivity extends Activity {
 
-
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_info);
         addUUID();
@@ -24,42 +21,51 @@ public class PhoneInfoActivity extends Activity {
         // Now read the desired content to a textview.
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
         String savedUUID = prefs.getString("app_uuid", " ");
-
         TextView loading_UUID = (TextView)findViewById(R.id.deviceid);
         loading_UUID.setText(savedUUID);
     }
 
     public void addEmail() {
-        // Now read the desired content to a textview.
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
         String email = prefs.getString("User_email", " ");
-
         TextView loading_email = (TextView)findViewById(R.id.usermail);
         loading_email.setText(email);
     }
     public void addServer() {
-        // Now read the desired content to a textview.
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
         String server = prefs.getString("Server_URL", " ");
-
         TextView loading_Server = (TextView)findViewById(R.id.server);
         loading_Server.setText(server);
     }
     public void addStartTime() {
-        // Now read the desired content to a textview.
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
-        String startTime = prefs.getString("App_Start_Time", " ");
-
-        TextView loading_Server = (TextView)findViewById(R.id.startTime);
-        loading_Server.setText(startTime);
+        Integer hoursStart = prefs.getInt("startHour", 00);
+        Integer minutesStart = prefs.getInt("startMinute", 00);
+        StringBuilder sb = new StringBuilder();
+        sb.append(hoursStart).append(":");
+        if (minutesStart<10){
+            sb.append("0");
+            sb.append(minutesStart);
+        }
+        else{sb.append(minutesStart);}
+        String startTime = sb.toString();
+        TextView loading_StartTime = (TextView)findViewById(R.id.startTime);
+        loading_StartTime.setText(startTime);
     }
     public void addStopTime() {
-        // Now read the desired content to a textview.
         SharedPreferences prefs = getSharedPreferences("AdlusPrefsFile", MODE_PRIVATE);
-        String stopTime = prefs.getString("App_Stop_Time", " ");
-
-        TextView loading_Server = (TextView)findViewById(R.id.stopTime);
-        loading_Server.setText(stopTime);
+        Integer hoursStop = prefs.getInt("stopHour", 00);
+        Integer minutesStop = prefs.getInt("stopMinute", 00);
+        StringBuilder sb = new StringBuilder();
+        sb.append(hoursStop).append(":");
+        if (minutesStop<10){
+            sb.append("0");
+            sb.append(minutesStop);
+        }
+        else{sb.append(minutesStop);}
+        String stopTime = sb.toString();
+        TextView loading_StopTime = (TextView)findViewById(R.id.stopTime);
+        loading_StopTime.setText(stopTime);
     }
 
 }
