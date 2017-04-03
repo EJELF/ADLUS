ef43b93309fec3162de421b659df4ade396a172a
Edgars Janovskis <edgars@enclatvia.lv>
2017-04-03 21:43:06 +0300

    Services branch

---
+++ app/src/main/java/com/android/edgarsjanovskis/adlus/LocationService.java
@@ -0,0 +1,15 @@
+package com.android.edgarsjanovskis.adlus;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocationService extends Service {
+    public LocationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+       return null;
+    }
+}
